13afdcbb61702d6efc986c6676a7c19769b14d39
Kruno Petrik <petrik.kpetrik@gmail.com>
2022-02-02 20:26:50 +0100

    Finished the and added the ability to annonate all files

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
+from json import load
 import pandas as pd
 import numpy as np
+import os
 
-
-def load_annotations_from_file(filename):
-    data = pd.read_csv('datasets\CARPK_devkit\data\Annotations\\'+filename, sep=" ", header=None)
+def load_annotations_from_file(file_path):
+    data = pd.read_csv(file_path, sep=" ", header=None)
     return data
 
 def convert_annotations_to_opencv_compatible(data:pd.DataFrame, filename):
@@ -11,9 +12,16 @@ def convert_annotations_to_opencv_compatible(data:pd.DataFrame, filename):
     data=data.drop(data.columns[[4]], axis=1)
     out=data.values.ravel('F')
     out=np.concatenate(([num_of_detected_cars],out))
-    f=open("Annotations\\"+filename[0:-4]+".txt", "w")
-    f.write(filename+" ")
+    f=open("Annotations\\"+filename, "w")
+    f.write("datasets\CARPK_devkit\data\Images"+filename[0:-4]+".png"+" ")
     np.savetxt(f, out, delimiter=" ", fmt="%d", newline=' ')
     f.close()
     return data
 
+path="datasets\CARPK_devkit\data\Annotations\\"
+
+with os.scandir(path) as it:
+    for entry in it:
+        if entry.name.endswith(".txt") and entry.is_file():
+            data = load_annotations_from_file(entry.path)
+            convert_annotations_to_opencv_compatible(data, entry.name)
