df7eb9ed04b87b60cd72304098baa2077b4dc347
Kruno Petrik <petrik.kpetrik@gmail.com>
2022-02-22 19:56:01 +0100

    cascade 4, stage 10

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from time import sleep
 import cv2 as cv
 from functions import *
 
-car_cascade=cv.CascadeClassifier('cascade3\\cascade.xml')
+car_cascade=cv.CascadeClassifier('cascade4\\cascade.xml')
 frame=cv.imread("test_img.png")
 frame=cv.resize(frame, (640,480), interpolation=cv.INTER_AREA)
-detectAndDisplay(frame, car_cascade)
+cars=detectAndDisplay(frame, car_cascade)
+print("Detected ", len(cars), " cars.")
 cv.waitKey()
