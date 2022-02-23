564b1c668bc7434d49734f82765157ddd62d5eb8
Kruno Petrik <petrik.kpetrik@gmail.com>
2022-02-23 15:31:11 +0100

    loaded a different cascade

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from time import sleep
 import cv2 as cv
 from functions import *
 
-car_cascade=cv.CascadeClassifier('cascade4\\cascade.xml')
-frame=cv.imread("test_img.png")
-frame=cv.resize(frame, (640,480), interpolation=cv.INTER_AREA)
+car_cascade=cv.CascadeClassifier('cascade_final_32_19_optimized\\cascade.xml')
+frame=cv.imread("20160524_GF1_00149.png")
+#frame=cv.resize(frame, (640,480), interpolation=cv.INTER_AREA)
 cars=detectAndDisplay(frame, car_cascade)
 print("Detected ", len(cars), " cars.")
 cv.waitKey()
