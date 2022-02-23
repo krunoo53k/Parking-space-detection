f4b2c78603c2ce05962e72bac4c9480b337ff26c
Kruno Petrik <petrik.kpetrik@gmail.com>
2022-02-23 14:03:06 +0100

    loaded a different image

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from functions import *
 w24h24=cv.CascadeClassifier('cascades_test\w24h24\\cascade.xml')
 w32h19=cv.CascadeClassifier('cascades_test\w32h19\\cascade.xml')
 w48h27=cv.CascadeClassifier('cascades_test\w48h27\\cascade.xml')
-frame=cv.imread("test_img.png")
+frame=cv.imread("test_img2.png")
 #frame=cv.resize(frame, (640,480), interpolation=cv.INTER_AREA)
 cars=detectAndDisplay(frame, w24h24)
 print("Detected ", len(cars), " cars.")
