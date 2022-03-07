2b74c5b007742b97fd2317e69b307781cff71c8c
Kruno Petrik <petrik.kpetrik@gmail.com>
2022-03-07 22:14:06 +0100

    Cleaned main function

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-from time import sleep
 import cv2 as cv
 from functions import *
 
-car_cascade=cv.CascadeClassifier('cascade_final_32_19_optimized\\cascade.xml')
-frame=cv.imread("20160524_GF1_00149.png")
-#frame=cv.resize(frame, (640,480), interpolation=cv.INTER_AREA)
-cars=detectObjects(frame, car_cascade, 24)
-print("Detected ", len(cars), " cars.")
-cv.waitKey()
