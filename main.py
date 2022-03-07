41592b445d39e581e23b8dc5cc5b5c67a9ed6689
Kruno Petrik <petrik.kpetrik@gmail.com>
2022-03-07 21:49:30 +0100

    Fixed detect and display discrepancy

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from functions import *
 car_cascade=cv.CascadeClassifier('cascade_final_32_19_optimized\\cascade.xml')
 frame=cv.imread("20160524_GF1_00149.png")
 #frame=cv.resize(frame, (640,480), interpolation=cv.INTER_AREA)
-cars=detectAndDisplay(frame, car_cascade, 24)
+cars=detectObjects(frame, car_cascade, 24)
 print("Detected ", len(cars), " cars.")
 cv.waitKey()
