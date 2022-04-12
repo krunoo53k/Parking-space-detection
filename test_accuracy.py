2a4cfd33d9fc19c0e1bcc404893aaaf16bb9911e
Kruno Petrik <petrik.kpetrik@gmail.com>
2022-04-12 19:46:28 +0200

    First version of accuracy test

--- test_accuracy.py
+++ test_accuracy.py
@@ -1,12 +1,27 @@
+from numpy import zeros
 from functions import *
-import copy
+import pandas as pd
 
 cascades_list, cascades_names=getCascadesAndNames()
 test_images, test_images_names=getTestImagesAndNames()
 
+cascades_data=pd.DataFrame(cascades_names)
+cascades_data.insert(1, "actual_num",0)
+cascades_data.insert(1, "detected_num",0)
+cascades_data["detected_num"] = pd.to_numeric(cascades_data["detected_num"])
+cascades_data["actual_num"] = pd.to_numeric(cascades_data["actual_num"])
+
+print(cascades_data)
+
 for (frame, image_name) in zip(test_images, test_images_names):
     print("Detecting on image: ", image_name)
+    row=0
     for (cascade_filter, cascade_name) in zip(cascades_list, cascades_names):
-        displayed_image=copy.deepcopy(frame)
-        detected_objects=detectObjects(displayed_image,cascade_filter, 24)
-        print("Cascade '",cascade_name,"' found ", len(detected_objects), "out of ", getNumOfCarsOnImage(image_name, "Annotations\pos.txt"), " cars.")
+        detected_objects=detectObjects(frame,cascade_filter, 24)
+        actual_number_of_cars=getNumOfCarsOnImage(image_name, "Annotations\pos.txt")
+        print("Cascade '",cascade_name,"' found ", len(detected_objects), "out of ", actual_number_of_cars, " cars.")
+        cascades_data["detected_num"][row]+=len(detected_objects)
+        cascades_data["actual_num"][row]+=actual_number_of_cars
+        row+=1    
+    print(cascades_data)
+    
