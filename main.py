a2262169e2826b6793f6d1321015d6cce8b02194
Kruno Petrik <petrik.kpetrik@gmail.com>
2022-04-12 18:11:09 +0200

    testing purposes

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import cv2 as cv
 from functions import *
 
-print(getCarNumOfImage("20160524_GF2_00061.png", "Annotations\pos.txt"))
+cascade=cv.CascadeClassifier("cascades\cascade_48_27_lbp\cascade.xml")
+frame=cv.imread("test_images\\20160524_GF2_00061.png")
+detected_objects=detectObjects(frame, cascade, 24)
+displayObjects("xD",frame, detected_objects)
+cv.waitKey()
